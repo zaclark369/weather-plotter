80bf0124c6677468c0d3708bf4a0ff12572a05b7
Zachary Clark <zacclark369@gmail.com>
2021-12-19 20:38:46 -0800

    readme and more bugfixes

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # weather-plotter
+
+## Purpose
+A simple application that shows a day to day weather forecast. You can search by city and it will give a visual of what the weather looks like in an intuitive image.
+
+## Built with
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+* openweather API
+
+## Website
+https://zaclark369.github.io/weather-plotter/
+
+## Screenshot
+The websites landing page.
+   ![screenshot of the website](assets/images/screencapture.png)
+
+
+## Contribution
+Made by Zachary Clark
+* https://github.com/zaclark369
+
+### ©️2021 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,128 +1,125 @@
-const searchEl = document.getElementById('main');
-const navEl = document.getElementById('nav-column');
-const apiKey = 'e50a47ec15988784ce45ae496cc6fe45';
-let cityEl = '';
-const searchBtn = document.getElementById('searchbtn');
-
+const searchEl = document.getElementById("main");
+const navEl = document.getElementById("navigation");
+const apiKey = "e50a47ec15988784ce45ae496cc6fe45";
+let cityEl = "";
+const searchBtn = document.getElementById("searchbtn");
 
 const retrieveWeather = (apiUrl) => {
-    fetch(apiUrl)
-      .then((response) => {
-        if (response.status === 404) {
-          const headerEl = document.createElement('h1');
-          headerEl.id = 'error-msg';
-          headerEl.textContent = 'Data not found. Please enter a city and try again';
-          navEl.appendChild(headerEl);
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.status === 404) {
+        const headerEl = document.createElement("h1");
+        headerEl.id = "error-msg";
+        headerEl.textContent =
+          "Data not found. Please enter a city and try again";
+        navEl.appendChild(headerEl);
+        return;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const cityName = data.name;
+      if (!localStorage.getItem("search-history")) {
+        const storageArray = [cityName];
+        localStorage.setItem("search-history", JSON.stringify(storageArray));
+        const headerEl = document.createElement("h1");
+        headerEl.classList.add("bg-primary", "text-center", "search-item");
+        headerEl.style = "cursor:auto; padding: 2px";
+        headerEl.textContent = cityName;
+        navEl.appendChild(headerEl);
+        headerEl.addEventListener("click", (event) => {
+          userCity = event.target.textContent;
+          const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
+          retrieveWeather(weatherApi);
+        });
+      }
+
+      const updateLocalStorage = () => {
+        const storageArray = JSON.parse(localStorage.getItem("search-history"));
+
+        while (
+          storageArray[storageArray.length - 1] !== cityName &&
+          storageArray.length
+        ) {
+          storageArray.pop();
+        }
+        if (!storageArray.length) {
+          const newArray = JSON.parse(localStorage.getItem("search-history"));
+          newArray.push(cityName);
+          localStorage.setItem("search-history", JSON.stringify(newArray));
+        } else {
           return;
         }
-        return response.json();
-      })
-      .then((data) => {
-        const cityName = data.name;
-        if (!localStorage.getItem('search-history')) {
-          const storageArray = [cityName];
-          localStorage.setItem('search-history', JSON.stringify(storageArray));
-          const headerEl = document.createElement('h1');
-          headerEl.classList.add(
-            'bg-primary',
-            'text-center',
-            'search-item'
-          );
-          headerEl.style = 'cursor:auto; padding: 2px';
-          headerEl.textContent = cityName;
-          navEl.appendChild(headerEl);
-          headerEl.addEventListener('click', (event) => {
-            userCity = event.target.textContent;
-            const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
-            fetchWeather(weatherApi);
-          });
-        }
+      };
 
-        const updateLocalStorage = () => {
-            const storageArray = JSON.parse(localStorage.getItem('search-history'));
+      updateLocalStorage();
+      handleStorage();
+      // cityEl = data.name;
 
-            while (storageArray[storageArray.length-1] !== cityName && storageArray.length) {
-                storageArray.pop();
+      if (document.getElementById("error-msg")) {
+        const errorArray = Array.from(document.querySelectorAll("#error-msg"));
+        errorArray.forEach((item) => item.remove());
+      }
+
+      const apiCall = (data) => {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}&units=imperial`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (document.getElementById("today")) {
+              document.getElementById("today").remove();
             }
-            if(!storageArray.length) {
-                const newArray = JSON.parse(localStorage.getItem('search-history'));
-                newArray.push(cityName);
-                localStorage.setItem('search-history', JSON.stringify(newArray));
-            }
-            else
-            {
-                return;
-            }
-        };
 
-        updateLocalStorage();
-        handleStorage();
-        // cityEl = data.name;
-
-        if (document.getElementById('error-msg')) {
-            const errorArray = Array.from(document.querySelectorAll('#error-msg'));
-            errorArray.forEach((item) => item.remove());
-        }
-
-        const apiCall = (data) => {
-            fetch(
-                `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}&units=imperial`
-            )
-            .then((response) => response.json())
-            .then((data) => {
-                if (document.getElementById('today')) {
-                    document.getElementById('today').remove();
-                }
-
-                const currentDate = new Date(data.current.dt *1000);
-                const month = currentDate.getMonth() + 1;
+            const currentDate = new Date(data.current.dt * 1000);
+            const month = currentDate.getMonth() + 1;
             const day = currentDate.getDate();
             const year = currentDate.getFullYear();
-            const todayEl = document.createElement('section');
-                todayEl.classList.add('container', 'col-12', 'col-md-12');
-                todayEl.id = 'today';
-                const dayContainer = document.createElement('div');
-                dayContainer.classList.add('row');
+            const todayEl = document.createElement("section");
+            todayEl.classList.add("container", "col-12", "col-md-12");
+            todayEl.id = "today";
+            const dayContainer = document.createElement("div");
+            dayContainer.classList.add("row");
 
-                const dateEl = document.createElement('h2');
-                dateEl.textContent = '${month}/${day}/${year}';
-                todayEl.appendChild(dateEl);
-                document.getElementById('first-row').appendChild(todayEl);
-                
-                const locationEl = document.createElement('h2');
-                locationEl.textContent = cityEl;
-                
-                const localWeatherEl = document.createElement('h3');
-                localWeatherEl.textContent = data.current.weather[0].description;
-                localWeatherEl.classList.add('col-4');
+            const dateEl = document.createElement("h2");
+            dateEl.textContent = "${month}/${day}/${year}";
+            todayEl.appendChild(dateEl);
+            document.getElementById("first-row").appendChild(todayEl);
 
-                const weatherImage = document.createElement('img');
-                weatherImage.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
-                weatherImage.classList.add('bg-light', 'col-2', 'float-end');
+            const locationEl = document.createElement("h2");
+            locationEl.textContent = cityEl;
 
-                dayContainer.appendChild(locationEl);
-                dayContainer.appendChild(localWeatherEl);
-                dayContainer.appendChild(weatherImage);
+            const localWeatherEl = document.createElement("h3");
+            localWeatherEl.textContent = data.current.weather[0].description;
+            localWeatherEl.classList.add("col-4");
 
-                const todayTemperatureEl = document.createElement('h4');
-                todayTemperatureEl.textContent = `Temperature (Fahrenheit): ${data.current.temp}`;
-                const todayWindEl = document.createElement('h4');
-               todayWindEl.textContent = `Wind: ${data.current.wind_speed}mph`;
-                const todayHumidityEl = document.createElement('h4');
-                todayHumidityEl.textContent = `Humidity: ${data.current.humidity}`;
+            const weatherImage = document.createElement("img");
+            weatherImage.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+            weatherImage.classList.add("bg-light", "col-2", "float-end");
 
-                const ultravioletEl = document.createElement('h4');
-                ultravioletEl.textContent = `UV Index: ${data.current.uvi}`;
-            ultravioletEl.classList.add('rounded', 'border', 'border-1');
-            ultravioletEl.style.width = 'fit-content';
-            ultravioletEl.style.padding = '15px';
+            dayContainer.appendChild(locationEl);
+            dayContainer.appendChild(localWeatherEl);
+            dayContainer.appendChild(weatherImage);
+
+            const todayTemperatureEl = document.createElement("h4");
+            todayTemperatureEl.textContent = `Temperature (Fahrenheit): ${data.current.temp}`;
+            const todayWindEl = document.createElement("h4");
+            todayWindEl.textContent = `Wind: ${data.current.wind_speed}mph`;
+            const todayHumidityEl = document.createElement("h4");
+            todayHumidityEl.textContent = `Humidity: ${data.current.humidity}`;
+
+            const ultravioletEl = document.createElement("h4");
+            ultravioletEl.textContent = `UV Index: ${data.current.uvi}`;
+            ultravioletEl.classList.add("rounded", "border", "border-1");
+            ultravioletEl.style.width = "fit-content";
+            ultravioletEl.style.padding = "15px";
             if (data.current.uvi > 2) {
-              ultravioletEl.classList.add('bg-warning');
+              ultravioletEl.classList.add("bg-warning");
             }
             if (data.current.uvi > 5) {
-              ultravioletEl.classList.add('bg-danger');
+              ultravioletEl.classList.add("bg-danger");
             } else {
-              ultravioletEl.classList.add('bg-success');
+              ultravioletEl.classList.add("bg-success");
             }
 
             todayEl.appendChild(dayContainer);
@@ -131,28 +128,28 @@ const retrieveWeather = (apiUrl) => {
             todayEl.appendChild(todayHumidityEl);
             todayEl.appendChild(ultravioletEl);
 
-            const forecastHandle =  (data) => {
-                if (document.getElementById('forecast-container')) {
-                    document.getElementById('forecast-container').remove();
-                }
-                const forecastContainerEl = document.createElement('div');
-                forecastContainerEl.classList.add('container-fluid');
-              forecastContainerEl.id = 'forecast-container';
-              const forecastRowEl = document.createElement('div');
-              forecastRowEl.classList.add('row');
+            const forecastHandle = (data) => {
+              if (document.getElementById("forecast-container")) {
+                document.getElementById("forecast-container").remove();
+              }
+              const forecastContainerEl = document.createElement("div");
+              forecastContainerEl.classList.add("container-fluid");
+              forecastContainerEl.id = "forecast-container";
+              const forecastRowEl = document.createElement("div");
+              forecastRowEl.classList.add("row");
 
               const forecastArray = [];
-              for (let i = 0; i < 4; i++) {
-                  forecastArray.push(data.daily[i]);
+              for (let i = 0; i < 6; i++) {
+                forecastArray.push(data.daily[i]);
               }
 
               const createCards = (forecast) => {
-                  if (!forecast.length) {
-                      return;
-                  }
-                  const day = forecast.shift();
-                  const date = new Date(day.dt * 1000);
-                const dateEl = document.createElement('h3');
+                if (!forecast.length) {
+                  return;
+                }
+                const day = forecast.shift();
+                const date = new Date(day.dt * 1000);
+                const dateEl = document.createElement("h3");
                 dateEl.textContent = `${
                   date.getMonth() + 1
                 }/${date.getDate()}/${date.getFullYear()}`;
@@ -160,25 +157,24 @@ const retrieveWeather = (apiUrl) => {
                 forecastContainerEl.appendChild(forecastRowEl);
                 mainEl.appendChild(forecastContainerEl);
 
-
-                const cardColumn = document.createElement('div');
-                cardColumn.classList.add('col-12');
-                const cardEl = document.createElement('div');
-                cardEl.classList.add('card', 'bg-dark', 'border');
-                const cardImg = document.createElement('img');
-                cardImg.classList.add('card-img-top');
+                const cardColumn = document.createElement("div");
+                cardColumn.classList.add("col-12");
+                const cardEl = document.createElement("div");
+                cardEl.classList.add("card", "bg-dark", "border");
+                const cardImg = document.createElement("img");
+                cardImg.classList.add("card-img-top");
                 cardImg.src = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
-                cardImg.style.width = '50%';
-                const cardBody = document.createElement('div');
-                cardBody.classList.add('card-body');
-                const cardTemp = document.createElement('h5');
-                cardTemp.classList.add('fs-5');
+                cardImg.style.width = "50%";
+                const cardBody = document.createElement("div");
+                cardBody.classList.add("card-body");
+                const cardTemp = document.createElement("h5");
+                cardTemp.classList.add("fs-5");
                 cardTemp.textContent = `Temp: ${day.temp.day} deg`;
-                const cardWind = document.createElement('h5');
-                cardWind.classList.add('fs-5');
+                const cardWind = document.createElement("h5");
+                cardWind.classList.add("fs-5");
                 cardWind.textContent = `Wind: ${day.wind_speed}mph`;
-                const cardHumidity = document.createElement('h5');
-                cardHumidity.classList.add('fs-5');
+                const cardHumidity = document.createElement("h5");
+                cardHumidity.classList.add("fs-5");
                 cardHumidity.textContent = `Humidity: ${day.humidity}`;
 
                 cardEl.appendChild(cardImg);
@@ -190,57 +186,58 @@ const retrieveWeather = (apiUrl) => {
                 forecastRowEl.appendChild(cardColumn);
                 cardColumn.appendChild(cardEl);
 
-                createForecast(forecast);
-            };
-            createForecast(forecastArray);
+                forecastHandle(forecast);
               };
-              createForecast(data);
-            });
-        };
-        onecall(data);
+              forecastHandle(forecastArray);
+            };
+            forecastHandle(data);
+          });
+      };
+      apiCall(data);
     });
 };
 
 function handleClick() {
-    const inputEl = document.getElementById('search-city');
+  const inputEl = document.getElementById("search-city");
   cityEl = inputEl.value;
+  console.log(cityEl);
   const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
-  fetchWeather(weatherApi);
+  retrieveWeather(weatherApi);
 }
 
 function keyFunction(event) {
-    if (event.keyCode === 13) {
-      const inputEl = document.getElementById('search-city');
-      userCity = inputEl.value;
-      const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
-      fetchWeather(weatherApi);
-    }
+  if (event.keyCode === 13) {
+    const inputEl = document.getElementById("search-city");
+    userCity = inputEl.value;
+    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
+    retrieveWeather(weatherApi);
   }
+}
 
-  searchBtn.onclick(handleClick);
-  document.getElementById('search-city').addEventListener('keyup', keyFunction);
+searchBtn.addEventListener("click", handleClick);
+document.getElementById("search-city").addEventListener("keyup", keyFunction);
 
-  const handleStorage = () => {
-      if (localStorage.getItem('search-history')) {
-          const searchItems = Array.from(document.querySelectorAll('.search-item'));
-          searchItems.forEach((item) => {
-              item.remove();
-          });
+const handleStorage = () => {
+  if (localStorage.getItem("search-history")) {
+    const searchItems = Array.from(document.querySelectorAll(".search-item"));
+    searchItems.forEach((item) => {
+      item.remove();
+    });
 
-          const historyEl = JSON.parse(localStorage.getItem('search-history'));
-          historyEl.forEach(function (item) {
-              const headerEl = document.createElement('h1');
-              headerEl.classList.add('bg-primary', 'text-center', "search-item");
-              headerEl.style = 'padding: 7px';
-              headerEl.textContent = item.title;
-              navEl.appendChild(headerEl);
-              headerEl.addEventListener('click', (event) => {
-                  cityEl = event.target.textContent;
-                  const weatherAPI= `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
-                  fetchWeather(weatherAPI);
-              });
-          });
-      }
-  };
+    const historyEl = JSON.parse(localStorage.getItem("search-history"));
+    historyEl.forEach(function (item) {
+      const headerEl = document.createElement("h1");
+      headerEl.classList.add("bg-primary", "text-center", "search-item");
+      headerEl.style = "padding: 7px";
+      headerEl.textContent = item;
+      navEl.appendChild(headerEl);
+      headerEl.addEventListener("click", (event) => {
+        cityEl = event.target.textContent;
+        const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
+        retrieveWeather(weatherAPI);
+      });
+    });
+  }
+};
 
-  handleStorage();
+handleStorage();
